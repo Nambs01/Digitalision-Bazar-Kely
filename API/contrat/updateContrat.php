2ab9302456097f03877deb8af32bfac51d7c705c
nambs <angelonambs@gmail.com>
2024-02-24 19:05:22 +0300

    premier commit

---
+++ API/contrat/updateContrat.php
@@ -0,0 +1,43 @@
+<?php
+    require "../connectDb.php";
+
+	$request = json_decode(file_get_contents("php://input"));
+
+    $res['error'] = true;
+
+    if($request != null){
+        
+        $numContrat = $request->numContrat;
+        $cinMarchand = $request->cinMarchand;
+        $codeActivite = $request->codeActivite;
+        $newDateContrat = $request->newDateContrat;
+        $debPayement = $request->debPayement;
+        $valide = $request->valide;
+        $oldDateContrat = $request->oldDateContrat;
+
+		$sql = "UPDATE contrat SET
+                    cinMarchand = '".$cinMarchand."', 
+                    codeActivite = '".$codeActivite."',
+                    dateContrat = '".$newDateContrat."',
+                    debPayement = '".$debPayement."',
+                    valide = '".$valide."' 
+                WHERE 
+                    numContrat = '".$numContrat."' AND dateContrat = '".$oldDateContrat."'
+                ";
+
+		if ($conn->query($sql) === TRUE) {
+            $res['error']   = false;
+            $res['message'] = "Modification bien effectué";
+            $res['errorMessage'] = "Aucune erreur";
+		} else {
+            $res['message'] = "Modification non effectué";
+            $res['errorMessage'] = "Error: " . $sql . "\n" . $conn->error;
+		}
+
+        $conn->close();
+    }else{
+        $res['message'] = "Une erreur est survenue";
+    }
+    
+    print(json_encode($res));
+?>
